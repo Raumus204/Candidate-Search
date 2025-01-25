6d43139926c1885b192f928b49ab1163b718357e
Raumus204 <enacra204@gmail.com>
2025-01-24 19:34:39 -0600

    added server

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const port = process.env.VITE_PORT || 3001;
+console.log(`Running on port ${port}`);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    port: Number(process.env.VITE_PORT) || 3001,  // This will use the VITE_PORT from .env or fall back to 3001
+  },
 });
