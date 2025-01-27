28599f3da3a8a48b2f0b4a3cb490e450f23b21f5
Raumus204 <enacra204@gmail.com>
2025-01-27 12:06:26 -0600

    changed port

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const port = process.env.VITE_PORT || 4000;
-console.log(`Running on port ${port}`);
+// const port = process.env.VITE_PORT || 4000;
+// console.log(`Running on port ${port}`);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
   server: {
-    port: Number(process.env.VITE_PORT) || 4000, 
+    port: Number(process.env.VITE_PORT) || 3001, 
   },
 });
